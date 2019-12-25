8926bef0542e710529de7eb50ba8d10a250b1e17
David Richardson <david@tankski.co.uk>
2019-12-25 23:10:25 +0000

    Make use of the Django login view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
-from dashboard.views import DashboardLoginView, DashboardIndexView
+from dashboard.views import DashboardIndexView
 
 urlpatterns = [
-    path('login/', DashboardLoginView.as_view(), name='dashboard_login'),
+    path('login/', LoginView.as_view(), name='dashboard_login'),
     path('', DashboardIndexView.as_view(), name='dashboard_index')
 ]
