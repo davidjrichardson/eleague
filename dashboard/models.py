9579154b8ba4928e6c5cdf0959f335ea36324403
David Richardson <david@tankski.co.uk>
2019-12-25 19:19:50 +0000

    Create a dashboard user model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class ELeagueUser(models.Model):
+    """
+    This class represents a user that signs into the website to submit scores. It is not associated with any score.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    university = models.TextField(unique=True)  # Only one user can be tied to a university
