749b98ceb641123ba79203919c1bde7960d00452
David <david@tankski.co.uk>
2021-06-20 15:04:11 +0100

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![License: MIT](https://img.shields.io/github/license/davidjrichardson/eleague)](LICENSE)
+# ELeague
+The ELeague is a online archery postal league for student archers in the UK.
+
+
+### License
+
+This project is licensed under the [MIT license](LICENSE).
---
+++ core/urls.py
@@ -0,0 +1,38 @@
+"""eleague URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+
+admin.site.site_header = "ELeague Admin"
+admin.site.site_title = "ELeague Admin"
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
+urlpatterns = (
+    [
+        path("sentry-debug/", trigger_error),
+        path("admin/", admin.site.urls),
+        path("auth/", include("authentication.urls")),
+        path("api/", include("api.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
